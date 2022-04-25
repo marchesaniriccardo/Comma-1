c1e7cc8bdd74478f3d9e49f8b04fd3deb8a59a5d
pcto5ID <102298887+pcto5ID@users.noreply.github.com>
2022-04-25 23:19:30 +0200

    Update env.php

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -15,7 +15,7 @@ if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
 
 if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
 if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'pcto.5id@gmail.com');
-if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'pcto_cl5ID');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', '');
 if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
 if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
 

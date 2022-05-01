9564e7cc7a698d28981f80b1da2715226c8d2efe
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-01 22:00:38 +0200

    Update navbar.php

--- assets/layouts/navbar.php
+++ assets/layouts/navbar.php
@@ -1,3 +1,14 @@
  <!-- controlla che l'user sia loggato -->
  <!-- inizio navbar -->
+<nav class='navbar'>
+    <div class='col-left' id='logout' name='logout'>
+        <a href='../logout/logout.php'><img src='../images/logout.png' alt='logout'></a>
+    </div>
+    <div class='col-md' id='logo' name='logo'>
+        <img scr='../images/logowhite.png' alt='logo'>
+    </div>
+    <div class='col-right' id='acc' name='acc'>
+        <a href=''><img src='../images/account.png' alt='account'></a>
+    </div>
+</nav>
  <!-- fine navbar -->

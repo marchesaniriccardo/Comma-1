9bf8883db42c44da596c2a31e04a2ee67ddfc38d
pcto5ID <102298887+pcto5ID@users.noreply.github.com>
2022-05-01 20:41:55 +0200

    Update index.php

---
+++ Gestione Profili Aziendali/index.php
@@ -0,0 +1,14 @@
+<?php
+    include "../assets/layouts/header.php";
+?>
+
+<!-- Left side column. -->
+<div class="col-2"></div>
+<!-- Center Column -->
+<div class="col-8"></div>
+<!-- Right side column. -->
+<div class="col-2"></div>
+
+<?php
+    include "../assets/layouts/footer.php";
+?>

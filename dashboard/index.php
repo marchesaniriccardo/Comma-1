87032f67f4c5981ce88f5dfc7a7aea278e9eb977
pcto5ID <102298887+pcto5ID@users.noreply.github.com>
2022-05-01 20:41:44 +0200

    Update index.php

---
+++ dashboard/index.php
@@ -0,0 +1,12 @@
+<?php
+    define("TITLE", "Dashboard");
+    include "../assets/layouts/header.php";
+?>
+
+<div class = "contatiner">
+
+</div>
+
+<?php
+    include "../assets/layouts/footer.php";
+?>

fae603e687764fbf927c4a71a0339a0cf8608dca
pcto5ID <102298887+pcto5ID@users.noreply.github.com>
2022-05-02 22:13:37 +0200

    Update pcto.sql

--- assets/setup/pcto.sql
+++ assets/setup/pcto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 01, 2022 alle 16:28
--- Versione del server: 10.4.14-MariaDB
--- Versione PHP: 7.2.34
+-- Creato il: Mag 02, 2022 alle 22:10
+-- Versione del server: 10.4.24-MariaDB
+-- Versione PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -21,38 +21,8 @@ SET time_zone = "+00:00";
 -- Database: `pcto`
 --
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `cliente`
---
-
-CREATE TABLE `cliente` (
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `nome` varchar(255) DEFAULT NULL,
-  `indirizzo` varchar(255) DEFAULT NULL,
-  `telefono` int(10) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `last_login_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `cliente`
---
-
-INSERT INTO `cliente` (`username`, `email`, `password`, `nome`, `indirizzo`, `telefono`) VALUES
-('bpm', 'bpm@gmail.com', 'B685D42CE77340828731371174CD0ACB27B545922CC0879D68A0728C7B731D26', 'BPM', 'Via Roma', '395944219889'),
-('fineco', 'fineco@gmail.com', '6AA9E68229A313AB3159AC013FF047F4C0D3025A7ECB8DC2684D57E135CC0902', 'Fineco', 'Via Prato', '394401354849'),
-('generali', 'generali@gmail.com', '0FC0A4C5A84C8FF37367F425FF833B0A2B1A0DE7C0507B02D42D8C3A184F6884', 'Generali', 'Via Monte', '39968599589'),
-('intesa_san_paolo', 'intesasanpaolo@gmail.com', '7130EE8446EA733B45F3AB778519F94848C64594170A3F7BB680C00939CBF6DC', 'Intesa San Paolo', 'Via Facciolati', '396700758821'),
-('mediolanum', 'mediolanum@gmail.com', '116AB34633F55FA29772F206F789F963B06FAAEB942EF1BC638599519A6DDE68', 'Mediolanum', 'Via Keil', '39713167775'),
-('revolut', 'revolut@gmail.com', '58F4182F405C683D31E2F8B2DD145363C1919B16E961873C70FC695B1CAE0D6F', 'Revolut', 'Via Vittorio Veneto', '39707813644'),
-('sella', 'sella@gmail.com', '643266AE8F3F8B60986C29E49C7525F40509A63DD23065ED7D3BFCE35EE46F71', 'Sella', 'Via Del Martirio', '39896465725'),
-('unicredit', 'unicredit@gmail.com', 'unicreditP', 'Unicredit', 'Via Gemola', '3982692405470');
+create schema pcto;
+use pcto;
 
 -- --------------------------------------------------------
 
@@ -69,6 +39,39 @@ CREATE TABLE `auth_tokens` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `expires_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `indirizzo` varchar(255) DEFAULT NULL,
+  `telefono` int(10) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `last_login_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `cliente`
+--
+
+INSERT INTO `cliente` (`username`, `email`, `password`, `nome`, `indirizzo`, `telefono`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`) VALUES
+('bpm', 'bpm@gmail.com', 'B685D42CE77340828731371174CD0ACB27B545922CC0879D68A0728C7B731D26', 'BPM', 'Via Roma', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('fineco', 'fineco@gmail.com', '6AA9E68229A313AB3159AC013FF047F4C0D3025A7ECB8DC2684D57E135CC0902', 'Fineco', 'Via Prato', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('generali', 'generali@gmail.com', '0FC0A4C5A84C8FF37367F425FF833B0A2B1A0DE7C0507B02D42D8C3A184F6884', 'Generali', 'Via Monte', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('intesa_san_paolo', 'intesasanpaolo@gmail.com', '7130EE8446EA733B45F3AB778519F94848C64594170A3F7BB680C00939CBF6DC', 'Intesa San Paolo', 'Via Facciolati', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('mediolanum', 'mediolanum@gmail.com', '116AB34633F55FA29772F206F789F963B06FAAEB942EF1BC638599519A6DDE68', 'Mediolanum', 'Via Keil', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('revolut', 'revolut@gmail.com', '58F4182F405C683D31E2F8B2DD145363C1919B16E961873C70FC695B1CAE0D6F', 'Revolut', 'Via Vittorio Veneto', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('sella', 'sella@gmail.com', '643266AE8F3F8B60986C29E49C7525F40509A63DD23065ED7D3BFCE35EE46F71', 'Sella', 'Via Del Martirio', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('unicredit', 'unicredit@gmail.com', 'unicreditP', 'Unicredit', 'Via Gemola', 2147483647, NULL, '2022-05-02 19:02:31', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -101,38 +104,44 @@ CREATE TABLE `impiegato` (
   `Matricola` char(16) NOT NULL,
   `Nome` varchar(50) DEFAULT NULL,
   `Cognome` varchar(50) DEFAULT NULL,
+  `Username` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
   `Tipo` varchar(50) DEFAULT NULL,
-  `Processo` varchar(50) DEFAULT NULL
+  `Processo` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `last_login_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `impiegato`
 --
 
-INSERT INTO `impiegato` (`Matricola`, `Nome`, `Cognome`, `Tipo`, `Processo`) VALUES
-('0000001', 'Giuseppe', 'Manni', 'Operaio', 'controllo qualita input'),
-('0000002', 'Pietro', 'Di Angelo', 'Operaio', 'controllo qualita input'),
-('0000003', 'Laura', 'Romani', 'Operaio', 'irrigidimento plastica'),
-('0000004', 'Rodrigo', 'Costa', 'Operaio', 'irrigidimento plastica'),
-('0000005', 'Luigi', 'De Cicco', 'Operaio', 'irrigidimento plastica'),
-('0000006', 'Francesco', 'Padovan', 'Operaio', 'stampaggio grafica'),
-('0000007', 'Angelo', 'Piazza', 'Operaio', 'stampaggio grafica'),
-('0000008', 'Maria', 'Baglio', 'Operaio', 'posizionamento degli elaborati'),
-('0000009', 'Anna', 'Cremonesi', 'Operaio', 'inserimento contactless'),
-('0000010', 'Giuseppina', 'Toscani', 'Operaio', 'laminazione'),
-('0000011', 'Angela', 'Longo', 'Operaio', 'separazione carte'),
-('0000012', 'Concetta', ' Esposito', 'Operaio', 'separazione carte'),
-('0000013', 'Giorgio', 'Carraro', 'Operaio', 'applicazione ologramma anticontraffazione'),
-('0000014', 'Paolo', 'Fox', 'Operaio', 'inserimento chip'),
-('0000015', 'Fabio', 'Martinelli', 'Operaio', 'inserimento chip'),
-('0000016', 'Francesca', 'Giordano', 'Operaio', 'inserimento banda magnetica'),
-('0000017', 'Matteo', 'Lombardi', 'Operaio', 'inserimento banda magnetica'),
-('0000018', 'Carlo', 'Barbieri', 'Operaio', 'stampa codice carta'),
-('0000019', 'Cinzia', 'Moretti', 'Operaio', 'logistica'),
-('0000020', 'Leonardo', 'Rinaldi', 'Operaio', 'logistica'),
-('0000021', 'Basilio', 'Celentano', 'Addetto al controllo qualita', NULL),
-('0000022', 'Fabiola', 'Denari', 'Addetto al controllo qualita', NULL),
-('0000023', 'Igor', 'Rampa', 'Addetto al controllo qualita', NULL);
+INSERT INTO `impiegato` (`Matricola`, `Nome`, `Cognome`, `Username`, `Password`, `Tipo`, `Processo`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`) VALUES
+('0000001', 'Giuseppe', 'Manni', 'giuseppe_manni', 'EFF0E93D9B1B8DE86F48B88C261A222BCE4F65B6E3ED19A12124913F34B38951', 'Operaio', 'controllo qualita input', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000002', 'Pietro', 'Di Angelo', 'pietro_angelo', '193CA2DCA9B38D230B00E3E6F90C080D19E15EA564C3D0A2F64C8918CDF822ED', 'Operaio', 'controllo qualita input', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000003', 'Laura', 'Romani', 'laura_romani', 'CFD715DB9F65CA8CE14184BC20A5F51BED592C5D5DC951032FFF65C220983D3E', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000004', 'Rodrigo', 'Costa', 'rodrigo_costa', 'F69F8FC89B7548FF0203C39470AD61CAEF36232BB11A924DE1385FC17E5B625F', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000005', 'Luigi', 'De Cicco', 'luigi_ciccio', '2543CDA7BFA2FC4C1297E91DA053A15316128B88BDCDFF9747E3C78530B1D919', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000006', 'Francesco', 'Padovan', 'francesco_padovan', 'BAE1474BE5ED291846D58B3638A8370532B5A4E7D2CA81E751D7119EF4F18E59', 'Operaio', 'stampaggio grafica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000007', 'Angelo', 'Piazza', 'angelo_piazza', '712E983D1F9103D34238A6347BC34CCBC49A2E4CD5EFC614C83B356987755651', 'Operaio', 'stampaggio grafica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000008', 'Maria', 'Baglio', 'maria_baglio', 'A581A7E95280E9BFB137D969D8C47C2FC7B854747A842C2BA3EC2C6B0E78D20E', 'Operaio', 'posizionamento degli elaborati', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000009', 'Anna', 'Cremonesi', 'anna_cremonesi', '1DAA07252B565FED430AB696519472494AA53F5C8127DFCC42BE17D81F1403E1', 'Operaio', 'inserimento contactless', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000010', 'Giuseppina', 'Toscani', 'giuseppina_toscani', '6D9D8FE9A441F78FD8EB2E285586FAAF54C8E11A8174BA0C53A3C0E1A62F2AC1', 'Operaio', 'laminazione', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000011', 'Angela', 'Longo', 'angela_longo', 'C91FC5AB927FA766B951967F37A00D13A7C461C224DFEA9B8328FF20F411AF82', 'Operaio', 'separazione carte', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000012', 'Concetta', ' Esposito', 'concetta_esposito', '7C51A9804E2DAAD41D5DE074D859441CC36048D4071D93023A7DCDF3FF786529', 'Operaio', 'separazione carte', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000013', 'Giorgio', 'Carraro', 'giorgio_carraro', '25E32A62A67556E6B381364D020C9344DD8AABA12A1D12A3C2D047C6512F1713', 'Operaio', 'applicazione ologramma anticontraffazione', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000014', 'Paolo', 'Fox', 'paolo_fox', '0D06887126CABE32A794727C847A169E5D9962D816AA919401947D7E48704CEC', 'Operaio', 'inserimento chip', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000015', 'Fabio', 'Martinelli', 'fabio_martinelli', '1DACD9F335D148B5CCE7AAB4F2AA8CADE7661C50E2253062B3BEF191A0782264', 'Operaio', 'inserimento chip', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000016', 'Francesca', 'Giordano', 'francesca_giordano', 'E6152480CFEEF866BB84503ED33E828F9D674D1F49A1DBA796D7B440CB5BADED', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000017', 'Matteo', 'Lombardi', 'matteo_lombardi', '551607A1B81F7E6F964B01E57BA5BB0F1E5DB2E374CB63F9DAED2805407A58A3', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000018', 'Carlo', 'Barbieri', 'carlo_barbieri', 'A6D09E4F725406CB9D6010E83B39B708AB099853BABB751C0B0840F8ACF3AA2B', 'Operaio', 'stampa codice carta', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000019', 'Cinzia', 'Moretti', 'cinzia_moretti', 'F8FBC7E94524BE8CC9A6F1707749CBCB59E4826F1455612504183755910E9A0E', 'Operaio', 'logistica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000020', 'Leonardo', 'Rinaldi', 'leonardo_rinaldi', 'D620D07C8743FDABC5995F1ED749BE21EA59E9B41FAE985631BB5F190AB0B67B', 'Operaio', 'logistica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000021', 'Basilio', 'Celentano', 'basilio_celentano', '96C9811127209B5DD23C5B1EC7EC69E23074C478BCE3FC99945C1575BF0A94F0', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000022', 'Fabiola', 'Denari', 'fabiola_denari', '0CB7E0D4D7A397614554381E0516BA74FD5F870A7056C5DD4E518CC6052F1E7A', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000023', 'Igor', 'Rampa', 'igor_rampa', '9755FEFC878A2990AD5123634FD9656E90557875350269EE095643E67BBFD481', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -323,6 +332,16 @@ CREATE TABLE `rilevamento_interno` (
   `Semilavorato` char(16) NOT NULL,
   `Data` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `rilevamento_interno`
+--
+
+INSERT INTO `rilevamento_interno` (`NC`, `Impiegato`, `Semilavorato`, `Data`) VALUES
+(1, '0000003', '0000000017', '2021-01-02'),
+(2, '0000004', '0000000001', '2020-06-15'),
+(3, '0000001', '0000000010', '2020-05-17'),
+(3, '0000002', '0000000016', '2021-11-29');
 
 -- --------------------------------------------------------
 
@@ -691,7 +710,7 @@ ALTER TABLE `risoluzione_output`
 --
 ALTER TABLE `verifica_input`
   ADD CONSTRAINT `verifica_input_ibfk_1` FOREIGN KEY (`NC`) REFERENCES `nc_input` (`Numero`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `verifica_input_ibfk_2` FOREIGN KEY (`Impiegato`) REFERENCES `fornitore` (`nome`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `verifica_input_ibfk_2` FOREIGN KEY (`Impiegato`) REFERENCES `fornitore` (`Nome`) ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `verifica_interna`

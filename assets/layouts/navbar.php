f5a7875a912afd9a76c16fa13497ab835bc103d9
pcto5ID <102298887+pcto5ID@users.noreply.github.com>
2022-05-02 23:56:18 +0200

    Update navbar.php

--- assets/layouts/navbar.php
+++ assets/layouts/navbar.php
@@ -1,11 +1,11 @@
- <!-- BACKEND controlla che l'user sia loggato -->
+<?php if(isset($_SESSION['auth'])) ?>
 
  <!-- inizio navbar -->
 
  <nav class="navbar navbar-expand-lg bg-transparent navbar-dark py-3 fixed-top">
      <div class="container-fluid">
          <div class="col-1 h-100 d-flex align-items-center justify-content-center">
-             <a href="#"><i class="bi bi-box-arrow-left"></i></a>
+             <a href="../Logout/index.php"><i class="bi bi-box-arrow-left"></i></a>
          </div>
          <div class="col-10 h-100">
              <div class="logo_image h-100 d-flex align-items-center justify-content-center">
